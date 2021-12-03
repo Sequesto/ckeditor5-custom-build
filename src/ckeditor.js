167ffc62e016b234e5ce118a7072fb89f1b238dd
joachimvandenabeele <joachim@sequesto.io>
2021-12-03 17:00:02 +0100

    Adapt to use trial version

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,10 +4,14 @@
  */
 import DecoupledDocumentEditor from "@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor.js";
 import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment.js";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
+import AutoImage from "@ckeditor/ckeditor5-image/src/autoimage.js";
 import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js";
 import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import CloudServices from "@ckeditor/ckeditor5-cloud-services/src/cloudservices.js";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import ExportWord from "@ckeditor/ckeditor5-export-word/src/exportword.js";
 import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js";
 import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
 import FontFamily from "@ckeditor/ckeditor5-font/src/fontfamily.js";
@@ -21,30 +25,42 @@ import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
 import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
 import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
 import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock.js";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
 import Link from "@ckeditor/ckeditor5-link/src/link.js";
 import List from "@ckeditor/ckeditor5-list/src/list.js";
+import ListStyle from "@ckeditor/ckeditor5-list/src/liststyle.js";
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed.js";
 import PageBreak from "@ckeditor/ckeditor5-page-break/src/pagebreak.js";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
-import StandardEditingMode from "@ckeditor/ckeditor5-restricted-editing/src/standardeditingmode.js";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js";
 import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
 import Subscript from "@ckeditor/ckeditor5-basic-styles/src/subscript.js";
 import Superscript from "@ckeditor/ckeditor5-basic-styles/src/superscript.js";
 import Table from "@ckeditor/ckeditor5-table/src/table.js";
+import TableCaption from "@ckeditor/ckeditor5-table/src/tablecaption.js";
+import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
+import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
 import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
+import TodoList from "@ckeditor/ckeditor5-list/src/todolist";
 import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
 import EditorWatchdog from "@ckeditor/ckeditor5-watchdog/src/editorwatchdog.js";
-import ExportWord from "@ckeditor/ckeditor5-export-word/src/exportword";
 
 class Editor extends DecoupledDocumentEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
   Alignment,
+  Autoformat,
+  AutoImage,
   Base64UploadAdapter,
   BlockQuote,
   Bold,
+  CloudServices,
   Essentials,
+  ExportWord,
   FontBackgroundColor,
   FontColor,
   FontFamily,
@@ -58,19 +74,27 @@ Editor.builtinPlugins = [
   ImageStyle,
   ImageToolbar,
   ImageUpload,
+  Indent,
+  IndentBlock,
   Italic,
   Link,
   List,
+  ListStyle,
+  MediaEmbed,
   PageBreak,
   Paragraph,
-  StandardEditingMode,
+  PasteFromOffice,
   Strikethrough,
   Subscript,
   Superscript,
   Table,
+  TableCaption,
+  TableCellProperties,
+  TableProperties,
   TableToolbar,
+  TextTransformation,
+  TodoList,
   Underline,
-  ExportWord,
 ];
 
 // Editor configuration.
@@ -80,27 +104,37 @@ Editor.defaultConfig = {
       "heading",
       "|",
       "fontSize",
-      "fontColor",
+      "fontFamily",
       "|",
+      "fontColor",
       "fontBackgroundColor",
       "|",
       "bold",
       "italic",
       "underline",
       "strikethrough",
+      "subscript",
+      "superscript",
       "|",
       "alignment",
       "|",
       "numberedList",
       "bulletedList",
       "|",
-      "insertTable",
+      "outdent",
+      "indent",
+      "|",
+      "todoList",
       "link",
+      "blockQuote",
+      "imageUpload",
+      "insertTable",
+      "horizontalLine",
+      "pageBreak",
       "|",
       "undo",
       "redo",
       "|",
-      "uploadImage",
       "exportWord",
     ],
   },
@@ -111,7 +145,6 @@ Editor.defaultConfig = {
       "imageStyle:inline",
       "imageStyle:block",
       "imageStyle:side",
-      "linkImage",
     ],
   },
   table: {
@@ -170,17 +203,6 @@ Editor.defaultConfig = {
   exportWord: {
     tokenUrl:
       "https://85277.cke-cs.com/token/dev/0333d731efc42c6da821668ee4ea93bda22b860c380960c6efec2d3d4b99",
-    fileName: "document.docx",
-    converterUrl: "https://docx-converter.cke-cs.com/v1/convert",
-    stylesheets: ["EDITOR_STYLES"],
-    converterOptions: {
-      format: "A4",
-      margin_top: "1in",
-      margin_bottom: "1in",
-      margin_right: "1in",
-      margin_left: "1in",
-    },
-    dataCallback: (editor) => editor.getData({ pagination: true }),
   },
 };
 
